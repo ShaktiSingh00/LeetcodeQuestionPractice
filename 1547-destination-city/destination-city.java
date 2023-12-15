b0cd46bfc9cc813da255b042fc912b9392990a30
ShaktiSingh00 <141671412+ShaktiSingh00@users.noreply.github.com>
2023-12-15 21:55:03 +0530

    Time: 2 ms (79.52%) | Memory: 43.3 MB (23.46%) - LeetSync

---
+++ 1547-destination-city/destination-city.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String destCity(List<List<String>> paths) {
+        Set<String> outgoingCities = new HashSet<>();
+
+        for (List<String> path : paths) {
+            outgoingCities.add(path.get(0));
+        }
+
+        for (List<String> path : paths) {
+            if (!outgoingCities.contains(path.get(1))) {
+                return path.get(1);
+            }
+        }
+
+        // This line should not be reached based on the problem constraints
+        return null;
+
+    }
+}
