384b3e17576f934adeb22bf6197c48893cef1cb8
ShaktiSingh00 <141671412+ShaktiSingh00@users.noreply.github.com>
2023-12-24 01:15:02 +0530

    Time: 2 ms (82.39%) | Memory: 45 MB (5.62%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -7,8 +7,9 @@ class Solution {
         
         for(int i=0; i<n; i++) {
             if(hm.containsKey(target-nums[i])) {
-                ans[0] = hm.get(target-nums[i]);
-                ans[1] = i;
+                // ans[1] = i;
+                ans[1] = hm.get(target-nums[i]);
+                ans[0] = i;
              return ans;
             }
             hm.put(nums[i], i);
