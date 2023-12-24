964265c368fa528866143e7c4d0c563316bbd027
ShaktiSingh00 <141671412+ShaktiSingh00@users.noreply.github.com>
2023-12-24 14:28:38 +0530

    Time: 4 ms (74.75%) | Memory: 42.4 MB (6.56%) - LeetSync

--- 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.java
+++ 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.java
@@ -1,32 +1,24 @@
 class Solution {
     public int minOperations(String s) {
-        int n = s.length();
-        
-        // Initialize counts for assuming the string starts with '0' and '1'
-        int countStartWithZero = 0;
-        int countStartWithOne = 0;
+          int countForAlternate0 = 0;
+        int countForAlternate1 = 0;
 
-        // Iterate through the string assuming it starts with '0'
-        for (int i = 0; i < n; i++) {
-            // If the expected character is not '0', increment the count
-            if (i % 2 == 0 && s.charAt(i) != '0') {
-                countStartWithZero++;
-            } else if (i % 2 == 1 && s.charAt(i) != '1') {
-                countStartWithZero++;
+        for (int i = 0; i < s.length(); i++) {
+            if (i % 2 == 0) {
+                if (s.charAt(i) == '0') {
+                    countForAlternate1++;
+                } else {
+                    countForAlternate0++;
+                }
+            } else {
+                if (s.charAt(i) == '1') {
+                    countForAlternate1++;
+                } else {
+                    countForAlternate0++;
+                }
             }
         }
 
-        // Iterate through the string assuming it starts with '1'
-        for (int i = 0; i < n; i++) {
-            // If the expected character is not '1', increment the count
-            if (i % 2 == 0 && s.charAt(i) != '1') {
-                countStartWithOne++;
-            } else if (i % 2 == 1 && s.charAt(i) != '0') {
-                countStartWithOne++;
-            }
-        }
-
-        // Return the minimum of the two counts
-        return Math.min(countStartWithZero, countStartWithOne);
+        return Math.min(countForAlternate0, countForAlternate1);
     }
 }
