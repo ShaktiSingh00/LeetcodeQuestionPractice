52db7a70650bf891fe6ecaeb8895031115d8cdd4
ShaktiSingh00 <141671412+ShaktiSingh00@users.noreply.github.com>
2023-12-27 01:41:32 +0530

    Time: 88 ms (18.48%) | Memory: 56.5 MB (21.65%) - LeetSync

--- 948-sort-an-array/sort-an-array.java
+++ 948-sort-an-array/sort-an-array.java
@@ -1,87 +1,84 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        /* Arrays.sort(nums);
-        return nums; */
+// //         /* Arrays.sort(nums);
+// //         return nums; */
       
-        // Bubble Sort
-     /*   int n=nums.length;
-        for(int i=0;i<n;i++){
-          for(int j=0; j<n-i-1;j++){
-            if(nums[j]>nums[j+1]){
-              swap(nums,j,j+1);
-            }
-          }
-        }
-        return nums;
-    }
-    public void swap(int[] arr, int i, int j){
-      int temp=arr[i];
-      arr[i] =arr[j];
-      arr[j]=temp;
-    }*/
+// //         // Bubble Sort
+// //      /*   int n=nums.length;
+// //         for(int i=0;i<n;i++){
+// //           for(int j=0; j<n-i-1;j++){
+// //             if(nums[j]>nums[j+1]){
+// //               swap(nums,j,j+1);
+// //             }
+// //           }
+// //         }
+// //         return nums;
+// //     }
+// //     public void swap(int[] arr, int i, int j){
+// //       int temp=arr[i];
+// //       arr[i] =arr[j];
+// //       arr[j]=temp;
+// //     }*/
 
-    //Selection SOrt
-    /* int n=nums.length;
-     for(int i=0;i<n;i++){
-       int min_idx=i;
-       for(int j=i+1 ;j<n;j++){
-         if(nums[j]<nums[min_idx]){
-           min_idx = j;
-         }
-       }
-        int temp = nums[min_idx]; 
-            nums[min_idx] = nums[i]; 
-            nums[i] = temp; 
-     }
-      return nums; */
+// //     //Selection SOrt
+// //     /* int n=nums.length;
+// //      for(int i=0;i<n;i++){
+// //        int min_idx=i;
+// //        for(int j=i+1 ;j<n;j++){
+// //          if(nums[j]<nums[min_idx]){
+// //            min_idx = j;
+// //          }
+// //        }
+// //         int temp = nums[min_idx]; 
+// //             nums[min_idx] = nums[i]; 
+// //             nums[i] = temp; 
+// //      }
+// //       return nums; */
 
 
-// Insertion sort 
-    //  int n=nums.length;
-    //  //int min = Integer.MIN_VALUE;
+// // // Insertion sort 
+// //     //  int n=nums.length;
+// //     //  //int min = Integer.MIN_VALUE;
 
-    //  for(int i=1;i<n;i++){
-    //    int key=nums[i];
-    //    int j=i-1;
-    //    // min = Math.min(min, nums[i]);
-    //     while(j>=0 && nums[j]>key){
-    //       nums[j+1]=nums[j];
-    //       j--;
-    //     }
+// //     //  for(int i=1;i<n;i++){
+// //     //    int key=nums[i];
+// //     //    int j=i-1;
+// //     //    // min = Math.min(min, nums[i]);
+// //     //     while(j>=0 && nums[j]>key){
+// //     //       nums[j+1]=nums[j];
+// //     //       j--;
+// //     //     }
 
-    //     nums[j+1]=key;
+// //     //     nums[j+1]=key;
 
-    //  }
-    //   return nums;
+// //     //  }
+// //     //   return nums;
 
-      int n=nums.length;
-      if(n==1){
-          return nums;
+// //     //  
+// //     int n=nums.length;
+// //     int low = 0;
+// //     int high = n-1;
+// //     while(low < high){
+// //         int pidx = partition(arr,low, high);
+// //         q
+// //     }
+//       int n = nums.length;
+//       int left = 0;
+//       int right = n-1;
+//       while(left < right){
+//           int part = partition(nums,left ,right);
+//            return sortArray(nums, left , part-1);
+//            return sortArray(nums, part+1, right);
+//       }
+      int n = nums.length;
+      Queue<Integer> pq = new PriorityQueue<>();
+      for(int no : nums){
+          pq.add(no);
       }
-      int mid = n/2;
-      int[] left = sortArray(Arrays.copyOfRange(nums,0,mid));
-    int[] right = sortArray(Arrays.copyOfRange(nums,mid,n));
+      for(int i=0;i<n;i++){
+          nums[i] = pq.poll();
+      }
+      return nums;
 
-    return mergeArray(left,right);
-    }
-
-    public int[] mergeArray(int[] left, int[] right){
-        int i=0,j=0,k=0;
-        int[] arr = new int[left.length + right.length];
-
-        while(i<left.length && j<right.length){
-            if(left[i] < right[j]){
-                arr[k++] = left[i++]; 
-            }else {
-                arr[k++] = right[j++];
-            }
-        } 
-        while(i<left.length){
-            arr[k++] = left[i++];
-        }
-        while(j<right.length){
-            arr[k++]= right[j++];
-        }
-        return arr;
     }
 }
