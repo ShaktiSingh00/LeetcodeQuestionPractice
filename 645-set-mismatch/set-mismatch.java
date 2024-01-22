ffe8fd0ec783d1c67e5022c7b77b13d2f9f555f9
ShaktiSingh00 <141671412+ShaktiSingh00@users.noreply.github.com>
2024-01-22 23:02:43 +0530

    Time: 11 ms (23.97%) | Memory: 45.8 MB (11.88%) - LeetSync

--- 645-set-mismatch/set-mismatch.java
+++ 645-set-mismatch/set-mismatch.java
@@ -1,22 +1,42 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
-        int[] arr = new int[2];
+        // int[] arr = new int[2];
+        // int n = nums.length;
+        // //int count = 0;
+        // for(int i = 1;i<=n;i++){
+        //      int count = 0;
+        //     for(int j=0;j<n;j++){
+        //         if(nums[j]==i){
+        //             count++;
+        //         }
+        //     }
+        //     if(count==2){
+        //         arr[0]=i;
+        //     }
+        //     if(count==0){
+        //         arr[1] = i;
+        //     }
+        // }
+
+        // return arr;
         int n = nums.length;
-        //int count = 0;
-        for(int i = 1;i<=n;i++){
-             int count = 0;
-            for(int j=0;j<n;j++){
-                if(nums[j]==i){
-                    count++;
-                }
-            }
-            if(count==2){
-                arr[0]=i;
-            }
-            if(count==0){
-                arr[1] = i;
-            }
+        int actual_sum = n*(n+1)/2;
+        int unique_sum = 0, array_sum=0;
+        int[] arr = new int[2];
+        Set<Integer> hset = new HashSet();
+        
+        for(int num : nums){
+            hset.add(num);
         }
+        for(int num:nums){
+            array_sum += num;
+        }
+        for(int num : hset){
+            unique_sum += num;
+        }
+
+        arr[1]=actual_sum - unique_sum;
+        arr[0] = array_sum - unique_sum;
 
         return arr;
     }
