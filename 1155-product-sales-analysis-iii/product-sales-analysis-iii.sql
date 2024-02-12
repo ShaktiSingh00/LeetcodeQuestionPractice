9ac9e9d4d61f7167c9957d53790817b6692f8af2
ShaktiSingh00 <141671412+ShaktiSingh00@users.noreply.github.com>
2024-02-12 18:53:04 +0530

    Time: 1531 ms (86.20%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -0,0 +1,7 @@
+-- select distinct Sales.product_id, min(year) as first_year, quantity , price from Sales 
+-- join Product on Sales.product_id = Product.product_id
+-- ;
+
+
+select product_id , year as first_year, quantity , price from Sales
+where (product_id, year ) in (select product_id, min(year) from Sales group by product_id );
