d97200a025a29b21b8fcb637f786496a7beb0b40
ShaktiSingh00 <141671412+ShaktiSingh00@users.noreply.github.com>
2024-02-20 15:58:39 +0530

    Time: 534 ms (94.34%) | Memory: 0B (100.00%) - LeetSync

--- 2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
+++ 2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
@@ -1,8 +1,3 @@
-# Write your MySQL query statement below
--- select employee_id from Employees
--- where salary < 30000 and manager_id = ( select employee_id from Employees where salary < 30000 and manager_id != employee_id);
-
-SELECT employee_id 
-FROM Employees
-WHERE salary < 30000  AND manager_id not IN (SELECT  employee_id FROM Employees)
-order by employee_id;
+SELECT EMPLOYEE_ID FROM EMPLOYEES
+WHERE SALARY <30000 AND MANAGER_ID NOT IN (SELECT EMPLOYEE_ID FROM EMPLOYEES)
+ORDER BY EMPLOYEE_ID; 
