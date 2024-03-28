8f013cc07dcef39d5abbdfed103596f4e277eee3
ShaktiSingh00 <141671412+ShaktiSingh00@users.noreply.github.com>
2024-03-28 10:58:34 +0530

    Time: 66 ms (81.05%) | Memory: 60.7 MB (44.65%) - LeetSync

--- 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.java
+++ 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.java
@@ -1,20 +1,30 @@
 class Solution {
     public int maxSubarrayLength(int[] nums, int k) {
-        int i=0,j=0;
+        int j=0;
         int n = nums.length;
         Map<Integer, Integer> hmap = new HashMap<>();
         int len = 0;
-        while(j<n){
-            hmap.put(nums[j], hmap.getOrDefault(nums[j],0)+1);
-            while(i < j && hmap.getOrDefault(nums[j],0) > k) {
-                hmap.put(nums[i], hmap.get(nums[i])-1);
-                if(hmap.get(nums[i])==0){
-                    hmap.remove(nums[i]);
-                }
-                i++;
+        // while(j<n){
+        //     hmap.put(nums[j], hmap.getOrDefault(nums[j],0)+1);
+        //     while(i < j && hmap.getOrDefault(nums[j],0) > k) {
+        //         hmap.put(nums[i], hmap.get(nums[i])-1);
+        //         if(hmap.get(nums[i])==0){
+        //             hmap.remove(nums[i]);
+        //         }
+        //         i++;
+        //     }
+        //     len = Math.max(len, j-i+1);
+        //     j++;
+        // }
+
+        // return len;
+        for(int i=0;i<n;i++){
+            hmap.put(nums[i], hmap.getOrDefault(nums[i],0)+1);
+            while(hmap.get(nums[i])>k){
+                hmap.put(nums[j],hmap.get(nums[j])-1);
+                j++;
             }
-            len = Math.max(len, j-i+1);
-            j++;
+            len = Math.max(len, i-j+1);
         }
 
         return len;
