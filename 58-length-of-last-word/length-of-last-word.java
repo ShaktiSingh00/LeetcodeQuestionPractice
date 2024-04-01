e19f882e5b73288cfd71b2c8c143a05d556ef4b2
ShaktiSingh00 <141671412+ShaktiSingh00@users.noreply.github.com>
2024-04-01 11:14:40 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (61.80%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -1,16 +1,31 @@
+// class Solution {
+//     public int lengthOfLastWord(String s) {
+//         int count=0;
+//          s = s.trim();
+//          for(int i=s.length()-1;i>=0;i--)
+//         {
+//            if(s.charAt(i) !=' ')
+//             {
+//                 count++;
+//             }
+//             else
+//             {
+//                 break;
+//             }
+//         }
+//         return count;
+//     }
+// }
 class Solution {
     public int lengthOfLastWord(String s) {
-        int count=0;
-        String st = s.trim();
-         for(int i=st.length()-1;i>=0;i--)
-        {
-           if(st.charAt(i) !=' ')
-            {
+        // int n = s.length();
+        s = s.trim();
+        int count = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) != ' ') {
                 count++;
-            }
-            else
-            {
-                break;
+            } else {
+                break; // Break the loop when encountering a space
             }
         }
         return count;
