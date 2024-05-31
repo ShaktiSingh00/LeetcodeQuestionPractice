3f7db3fa39322b00747c986f42f84fd2a225ca90
ShaktiSingh00 <141671412+ShaktiSingh00@users.noreply.github.com>
2024-05-31 16:52:18 +0530

    Time: 14 ms (78.10%) | Memory: 54.1 MB (69.49%) - LeetSync

--- 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.java
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.java
@@ -1,23 +1,38 @@
 class Solution {
     public int minSwaps(String s) {
-        int n = s.length();
-       // int count = 0;
-        Stack<Character> st = new Stack<>();
-        for(int i=0;i<n;i++){
+    //     int n = s.length();
+    //    // int count = 0;
+    //     Stack<Character> st = new Stack<>();
+    //     for(int i=0;i<n;i++){
+    //         char c = s.charAt(i);
+    //         if(c=='['){
+    //             st.push(c);
+    //             //count++;
+    //         }else{
+    //             if(st.isEmpty() || st.peek()==']'){
+    //                 st.push(c);
+    //             }else{
+    //                 st.pop();
+    //             }
+    //         }
+    //     }
+    //     int totalStack = st.size();
+    //     int closeBracket = totalStack/2;
+    //     return (closeBracket+1)/2;
+           int n = s.length();
+           int count=0, close = 0;
+           for(int i=0;i<n;i++){
             char c = s.charAt(i);
             if(c=='['){
-                st.push(c);
-                //ount++;
+                count++;
             }else{
-                if(st.isEmpty() || st.peek()==']'){
-                    st.push(c);
+                if(count <= 0){
+                    close++;
                 }else{
-                    st.pop();
+                    count--;
                 }
             }
-        }
-        int totalStack = st.size();
-        int closeBracket = totalStack/2;
-        return (closeBracket+1)/2;
+           }
+           return (close+1)/2;
     }
 }
