d32901303e37c7a4daed92b977ce282b1f6d579d
SHAKTI KUMAR SINGH <141671412+ShaktiSingh00@users.noreply.github.com>
2024-07-25 23:53:13 +0530

    Time: 28 ms (50.43%) | Memory: 57.1 MB (14.31%) - LeetSync

--- 948-sort-an-array/sort-an-array.java
+++ 948-sort-an-array/sort-an-array.java
@@ -1,84 +1,35 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-// //         /* Arrays.sort(nums);
-// //         return nums; */
+       int n = nums.length;
+       if (n <= 1) {
+            return nums; // Base case: arrays with 0 or 1 element are already sorted
+        }
       
-// //         // Bubble Sort
-// //      /*   int n=nums.length;
-// //         for(int i=0;i<n;i++){
-// //           for(int j=0; j<n-i-1;j++){
-// //             if(nums[j]>nums[j+1]){
-// //               swap(nums,j,j+1);
-// //             }
-// //           }
-// //         }
-// //         return nums;
-// //     }
-// //     public void swap(int[] arr, int i, int j){
-// //       int temp=arr[i];
-// //       arr[i] =arr[j];
-// //       arr[j]=temp;
-// //     }*/
+        int mid = n/2;
+        int[] left = sortArray(Arrays.copyOfRange(nums,0,mid));
+        int[] right = sortArray(Arrays.copyOfRange(nums,mid,n));
 
-// //     //Selection SOrt
-// //     /* int n=nums.length;
-// //      for(int i=0;i<n;i++){
-// //        int min_idx=i;
-// //        for(int j=i+1 ;j<n;j++){
-// //          if(nums[j]<nums[min_idx]){
-// //            min_idx = j;
-// //          }
-// //        }
-// //         int temp = nums[min_idx]; 
-// //             nums[min_idx] = nums[i]; 
-// //             nums[i] = temp; 
-// //      }
-// //       return nums; */
-
-
-// // // Insertion sort 
-// //     //  int n=nums.length;
-// //     //  //int min = Integer.MIN_VALUE;
-
-// //     //  for(int i=1;i<n;i++){
-// //     //    int key=nums[i];
-// //     //    int j=i-1;
-// //     //    // min = Math.min(min, nums[i]);
-// //     //     while(j>=0 && nums[j]>key){
-// //     //       nums[j+1]=nums[j];
-// //     //       j--;
-// //     //     }
-
-// //     //     nums[j+1]=key;
-
-// //     //  }
-// //     //   return nums;
-
-// //     //  
-// //     int n=nums.length;
-// //     int low = 0;
-// //     int high = n-1;
-// //     while(low < high){
-// //         int pidx = partition(arr,low, high);
-// //         q
-// //     }
-//       int n = nums.length;
-//       int left = 0;
-//       int right = n-1;
-//       while(left < right){
-//           int part = partition(nums,left ,right);
-//            return sortArray(nums, left , part-1);
-//            return sortArray(nums, part+1, right);
-//       }
-      int n = nums.length;
-      Queue<Integer> pq = new PriorityQueue<>();
-      for(int no : nums){
-          pq.add(no);
-      }
-      for(int i=0;i<n;i++){
-          nums[i] = pq.poll();
-      }
-      return nums;
-
+        return merge(left,right);
+    }
+    public static int[] merge(int[] first, int[] second){
+        int m = first.length;
+        int n = second.length;
+        int i = 0;
+        int j=0,k=0;
+        int[] arr = new int[m+n];
+        while(i<m && j<n){
+            if(first[i]<second[j]){
+                arr[k++]=first[i++];
+            }else{
+                arr[k++] = second[j++];
+            }
+        }
+        while(i<m){
+            arr[k++]=first[i++];
+        }
+        while(j<n){
+            arr[k++] = second[j++];
+        }
+        return arr;
     }
 }
