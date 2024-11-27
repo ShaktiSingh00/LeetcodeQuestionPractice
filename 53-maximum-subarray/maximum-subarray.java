2ef7ba0b9a6401c4d5f9b6dd93c23fce1a3163fc
SHAKTI KUMAR SINGH <141671412+ShaktiSingh00@users.noreply.github.com>
2024-11-27 20:52:16 +0530

    Time: 1 ms (99.40%) | Memory: 57.7 MB (5.72%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -5,14 +5,11 @@ class Solution {
         int max = Integer.MIN_VALUE;
         for(int i=0;i<n;i++){
             sum += nums[i];
-
             max = Math.max(sum,max);
-
             if(sum<0){
                 sum=0;
             }
         }
-
         return max;
     }
 }
