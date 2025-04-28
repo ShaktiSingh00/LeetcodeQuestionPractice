e9d4a46287303beb652cc4eaf73fc6769b2be530
SHAKTI KUMAR SINGH <141671412+ShaktiSingh00@users.noreply.github.com>
2025-04-29 00:18:35 +0530

    Time: 24 ms (75.14%) | Memory: 47.1 MB (14.99%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -1,73 +1,20 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        // // int n = nums.length;
-        // // int max = 0, sum = 0;
-        // // for(int i=0;i<n;i++)
-        // // {
-        // //     for(int j=i;j<n;j++){
-        // //         sum += nums[j];
+        int n = nums.length;
+        int sum=0;
+        int count=0;
+        HashMap<Integer,Integer> hmap = new HashMap<>();
+        for(int i=0;i<n;i++){
+            sum += nums[i];
+            if(sum==k){
+                count++;
+            }
+            if(hmap.containsKey(sum-k)){
+                count += hmap.get(sum-k);
+            }
 
-        // //         if(sum==k){
-        // //         max = Math.max(max,j-i+1);
-        // //     }
-        // //     }
-        // // }
-        // // return max;
-
-        // // int n = nums.length;
-        // // int i=0,j=0,sum =nums[0];
-        // // int count=0;
-        // //int max = Integer.MIN_VALUE;
-        // // while(j<n){
-        // //     //sum += nums[j];
-        // //     while(i<=j && sum>k){
-        // //         sum -= nums[i];
-        // //         i++;
-        // //     }
-        // //     if(sum==k){
-        // //         count++;
-        // //     }
-        // //     j++;
-        // //     if (j < n) sum += nums[j];
-
-        // // }
-        // // return count;
-        
-        // int n = nums.length;
-        // int sum =0;
-        // int max = 0;
-        // Map<Integer, Integer> hmap = new HashMap<>();
-        // for(int i=0;i<n;i++){
-        //     sum += nums[i];
-        //     if(sum == k){
-        //         max = Math.max(max, i+1);
-        //     }
-        //     int rem = sum-k;
-        //     if(hmap.containsKey(rem)){
-        //         int len = i-hmap.get(rem);
-        //     }else{
-        //         hmap.put(sum,i);
-        //     }
-
-        // }
-        // return max;
-
-
-    int n = nums.length;
-     int count =0;
-     int sum = 0;
-     Map<Integer, Integer> hmap = new HashMap<>();
-     for(int i=0;i<n;i++){
-         sum += nums[i];
-         if(sum ==k){
-             count++;
+            hmap.put(sum, hmap.getOrDefault(sum, 0)+1);
         }
-        if(hmap.containsKey(sum-k)){
-            count += hmap.get(sum-k);
-        }
-        hmap.put(sum,hmap.getOrDefault(sum,0)+1);
-     }
-     return count;
-
+        return count;
     }
 }
