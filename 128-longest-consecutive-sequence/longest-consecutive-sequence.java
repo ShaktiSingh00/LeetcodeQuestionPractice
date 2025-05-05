45dfe9ecc2efde9a52df0ff38e156542b0584aef
SHAKTI KUMAR SINGH <141671412+ShaktiSingh00@users.noreply.github.com>
2025-05-06 00:42:37 +0530

    Time: 15 ms (99.79%) | Memory: 56.7 MB (86.31%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -1,56 +1,21 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
-        int n = nums.length;
-        int count = 0;
-        int max =0;
-        int lastsmall = Integer.MAX_VALUE;
         Arrays.sort(nums);
+        int n=nums.length;
+        int count=0;
+        int small = Integer.MAX_VALUE;
+        int max=0;
         for(int i=0;i<n;i++){
-            if(nums[i]-1 == lastsmall){
+            if(nums[i]-1==small){
                 count++;
-                lastsmall = nums[i];
-            }else if(nums[i] != lastsmall){
-                count = 1;
-                lastsmall = nums[i];
+                small=nums[i];
+            }else if(nums[i]!=small){
+                count=1;
+                small = nums[i];
             }
+            max=Math.max(max,count);
+        }  
 
-            max = Math.max(max,count);
-        }
-        return max;
+        return max; 
     }
 }
-//         // Arrays.sort(nums);
-//         // int longest = 0;
-//         // int count = 0;
-//         // int lastSmall = Integer.MIN_VALUE;
-//         // for(int i=0;i<n;i++){
-//         //     if(nums[i]-1 == lastSmall){
-//         //         count++;
-//         //         lastSmall = nums[i];
-//         //     }else if(nums[i] != lastSmall){
-//         //         count=1;
-//         //         lastSmall = nums[i];
-//         //     }
-
-//         //     longest = Math.max(longest, count);
-//         // }
-//         // return longest;
-//         Set<Integer> hset = new HashSet<>();
-//         int longest=0;
-//         for(int i=0;i<n;i++){
-//             hset.add(nums[i]);
-//         }
-//         for(int res : hset){
-//             if(!hset.contains(res-1)){
-//                 int count =1;
-//                 int x = res;
-//                 while(hset.contains(x+1)){
-//                     count++;
-//                     x = x+1;
-//                 }
-//                 longest = Math.max(longest, count);
-//             }
-//         }
-//         return longest;
-//     }
-// }
