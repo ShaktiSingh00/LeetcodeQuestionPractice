f3eabf655153b10e409a626546f0e2bd2a7f566b
SHAKTI KUMAR SINGH <141671412+ShaktiSingh00@users.noreply.github.com>
2025-07-01 23:31:51 +0530

    Time: 1 ms (87.19%) | Memory: 42.3 MB (26.45%) - LeetSync

---
+++ 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int possibleStringCount(String word) {
+        int n=word.length();
+        int count=1;
+        for(int i=0;i<n-1;i++){
+            if(word.charAt(i) == word.charAt(i+1)){
+                count++;
+            }
+        }
+        return count;
+    }
+}
