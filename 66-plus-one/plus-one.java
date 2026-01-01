09e774befcd125236655779da9f4697036c35eba
SHAKTI KUMAR SINGH <141671412+ShaktiSingh00@users.noreply.github.com>
2026-01-01 23:40:23 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (58.45%) - LeetSync

--- 66-plus-one/plus-one.java
+++ 66-plus-one/plus-one.java
@@ -1,43 +1,20 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        // int n = digits.length;
-        // //int[] arr = new int[n+1];
-        
-        // for(int i=n-1;i>=0;i--){
-        //     if(digits[n-1]!=9){
-        //         digits[i]++;
-        //         break;
-        //     }else{
-        //         digits[0]=0;
-        //     }
-        // }
-        // if(digits[0]==0){
-        //     int[] arr = new int[n+1];
-        //     arr[0]=1;
-        //     return arr;
-        // }
+        int n = digits.length;
 
-
-        // return digits;
-          int n=digits.length;
-        for(int i=n-1;i>=0;i--)
-        {
-            if(digits[i] != 9)
-            {
+        for(int i=n-1;i>=0;i--){
+            if(digits[i]!=9){
                 digits[i]++;
                 break;
-            }
-            else{
+            }else{
                 digits[i]=0;
             }
         }
-        if(digits[0] == 0)
-        {
+        if(digits[0]==0){
             int[] res = new int[n+1];
             res[0]=1;
             return res;
         }
-
-        return digits;   
+        return digits;
     }
 }
