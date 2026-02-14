a470b6b2aa46aa9be8950654343f2cf4fc56298e
SHAKTI KUMAR SINGH <141671412+ShaktiSingh00@users.noreply.github.com>
2026-02-15 00:55:43 +0530

    Time: 6 ms (79.11%) | Memory: 51.4 MB (21.59%) - LeetSync

--- 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.java
+++ 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.java
@@ -1,29 +1,35 @@
 class Solution {
+    public int maxKadane(int[] nums){
+        int max = Integer.MIN_VALUE;
+        int currSum = 0;
+        for(int i=0; i<nums.length; i++){
+            if(currSum<0) currSum = 0;
+            currSum += nums[i];
+            max = Math.max(max, currSum);
+        }
+        return max;
+    }
+    public int minKadane(int[] nums){
+        int min = Integer.MAX_VALUE;
+        int currSum = 0;
+        for(int i=0; i<nums.length; i++){
+            if(currSum>0) currSum = 0;
+            currSum += nums[i];
+            min = Math.min(min, currSum);
+        }
+        return min;
+    }
     public int maxSubarraySumCircular(int[] nums) {
         int n = nums.length;
-
-        int maxSum = nums[0];
-        int minSum = nums[0];
-        int max1Sum = nums[0];
-        int min1Sum = nums[0];
-        int totalSum = nums[0];
-
-        for(int i=1;i<n;i++){
-            max1Sum = Math.max(max1Sum+nums[i], nums[i]);
-            maxSum = Math.max(maxSum, max1Sum);
-
-            min1Sum = Math.min(min1Sum+nums[i], nums[i]);
-            minSum = Math.min(minSum, min1Sum);
-
-            totalSum += nums[i];
-        }
-
-        int circularSum = totalSum - minSum;
-
-        if(circularSum == 0){
-            return maxSum;
-        }
-
-        return Math.max(maxSum, circularSum);
+        int totalSum = 0;
+        for(int a : nums) totalSum += a;
+        
+        int maxSum = maxKadane(nums); // max subaaray sum (bina circular k hi max)
+        int minSum = minKadane(nums); // min subaray sum
+        int cirSum = totalSum - minSum; // circular max subaaray sum = total sum - min subarray sum
+        
+        if(maxSum<=0) return maxSum; // if all numbers are -ive
+        
+        return Math.max(maxSum, cirSum);
     }
 }
