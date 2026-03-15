4b332eabd670e3df216ab914c71323ff89760e52
SHAKTI KUMAR SINGH <141671412+ShaktiSingh00@users.noreply.github.com>
2026-03-16 00:40:22 +0530

    Time: 1 ms (88.13%) | Memory: 44.3 MB (53.80%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,19 +1,37 @@
+// class Solution {
+//     public List<List<Integer>> subsets(int[] nums) {
+//         int n=nums.length;
+//         List<List<Integer>> result = new ArrayList<>();
+//         generateSubset(nums,0,result, new ArrayList<>());
+//         return result;
+//     }
+//     public void generateSubset(int[] nums, int i,List<List<Integer>> res, List<Integer> r){
+//         if(i==nums.length){
+//             res.add(new ArrayList<>(r));
+//             return;
+//         }
+
+//         r.add(nums[i]);
+//         generateSubset(nums,i+1,res,r);
+//         r.remove(r.size()-1);
+//      //   generateSubset(nums,i+1,res,r);
+//     }
+// }
+
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        int n=nums.length;
-        List<List<Integer>> result = new ArrayList<>();
-        generateSubset(nums,0,result, new ArrayList<>());
-        return result;
+        List<List<Integer>> ans = new ArrayList<>();
+        backtrack(0, nums, new ArrayList<>(), ans);
+        return ans;
     }
-    public void generateSubset(int[] nums, int i,List<List<Integer>> res, List<Integer> r){
-        if(i==nums.length){
-            res.add(new ArrayList<>(r));
-            return;
-        }
 
-        r.add(nums[i]);
-        generateSubset(nums,i+1,res,r);
-        r.remove(r.size()-1);
-        generateSubset(nums,i+1,res,r);
+    void backtrack(int index, int[] nums, List<Integer> curr, List<List<Integer>> ans) {
+        ans.add(new ArrayList<>(curr));
+
+        for(int i = index; i < nums.length; i++) {
+            curr.add(nums[i]);              // choose
+            backtrack(i + 1, nums, curr, ans);
+            curr.remove(curr.size() - 1);   // backtrack
+        }
     }
 }
