153cb9f94ef1a92a09d83e54c91b611d28e65173
SHAKTI KUMAR SINGH <141671412+ShaktiSingh00@users.noreply.github.com>
2026-07-08 23:01:17 +0530

    Time: 1 ms (6.61%) | Memory: 43.5 MB (8.26%) - LeetSync

--- 2278-find-three-consecutive-integers-that-sum-to-a-given-number/find-three-consecutive-integers-that-sum-to-a-given-number.java
+++ 2278-find-three-consecutive-integers-that-sum-to-a-given-number/find-three-consecutive-integers-that-sum-to-a-given-number.java
@@ -1,13 +1,31 @@
 class Solution {
     public long[] sumOfThree(long num) {
-        if (num % 3 != 0) {
-            return new long[0];
+        // if (num % 3 != 0) {
+        //     return new long[0];
+        // }
+        
+        // // Find the middle number
+        // long mid = num / 3;
+        
+        // // Return the three consecutive integers sorted
+        // return new long[] {mid - 1, mid, mid + 1}; 
+        long low = 0;
+        long high = num;
+        
+        while (low <= high) {
+            long mid = low + (high - low) / 2;
+            long sum = 3 * mid; // The sum of (mid-1) + mid + (mid+1) is always 3 * mid
+            
+            if (sum == num) {
+                return new long[] {mid - 1, mid, mid + 1};
+            } else if (sum < num) {
+                low = mid + 1; // Look in the upper half
+            } else {
+                high = mid - 1; // Look in the lower half
+            }
         }
         
-        // Find the middle number
-        long mid = num / 3;
-        
-        // Return the three consecutive integers sorted
-        return new long[] {mid - 1, mid, mid + 1}; 
+        // If the loop finishes without finding a valid mid, no such integers exist
+        return new long[0];
     }
 }
